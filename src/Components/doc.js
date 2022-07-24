cf4e58668727d54d2e9fe9d880373890904712ff
Tasnim Mahi <tasnimmmahi@gmail.com>
2022-07-24 17:58:06 -0500

    cleaned doc.js

--- src/Components/doc.js
+++ src/Components/doc.js
@@ -1,32 +1,32 @@
 import { useState, useEffect } from "react";
-import ReactQuill from "react-quill"; // ES6
-import "react-quill/dist/quill.snow.css"; // ES6
-import { useNavigate, useParams } from "react-router-dom";
+
+import ReactQuill from "react-quill"; // used for the editor window
+import "react-quill/dist/quill.snow.css"; 
+import { useNavigate, useParams } from "react-router-dom"; // for changing pages 
 import { collection, doc, updateDoc, onSnapshot } from "firebase/firestore";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify"; // notification banners 
+import "react-toastify/dist/ReactToastify.css"; // notification banners
 
 export default function Editor({ database }) {
-  let navigate = useNavigate();
+  let navigate = useNavigate(); // changing pages 
+
   const [editorData, setEditorData] = useState("");
   const [title, setTitle] = useState(""); // to see the document title when you open a certain document
   let databaseCollection = collection(database, "docs-data");
   let params = useParams();
-  //console.log(params.id);
 
   const getEditorData = (value) => {
     setEditorData(value);
   };
   useEffect(() => {
-    const updateDocument = setTimeout(() => {
-      //console.log(editorData);
+    const updateDocument = setTimeout(() => { // automatically saves doc after typing something
       let docToUpdate = doc(databaseCollection, params.id);
       updateDoc(docToUpdate, {
         body: editorData
       })
         .then(() => {
-          toast.success("Document Updated", {
-            autoClose: 1000
+          toast.success("Document Updated", { // notify that document has been updated
+            autoClose: 1000  // closing banner after 1 sec
           });
         })
         .catch(() => {
@@ -37,18 +37,20 @@ export default function Editor({ database }) {
   }, [editorData]);
 
   useEffect(() => {
-    const document = doc(databaseCollection, params.id);
+    const document = doc(databaseCollection, params.id); // isntance of one doc
     onSnapshot(document, (docs) => {
-      setTitle(docs.data().title);
+      setTitle(docs.data().title); // Displaying the document title when doc is opened
       setEditorData(docs.data().body);
     });
   }, []);
 
+  
   return (
     <div>
       <div>
-        <button className="back-btn" onClick={() => navigate("/home")}>
-          Back
+        // Navigating back to the home page
+        <button className="back-btn" onClick={() => navigate("/home")}> 
+          Back 
         </button>
       </div>
       <ToastContainer />
