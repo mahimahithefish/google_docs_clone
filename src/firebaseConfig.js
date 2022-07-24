abd25625b6d77c288e6d5de24358832aed5691a6
Tasnim Mahi <tasnimmmahi@gmail.com>
2022-07-24 16:36:17 -0500

    commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore"; // access to firestore database
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
