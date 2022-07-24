35a37673728031a7d0a05224db30390358359df1
Tasnim Mahi <tasnimmmahi@gmail.com>
2022-07-24 16:35:00 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./styles.css";
 
-import Login from "./Components/Login";
-import Home from "./Components/HomePage";
-import Editor from "./Components/doc";
+import Login from "./Components/Login"; // For signing out and signing in
+import Home from "./Components/HomePage"; // After loging in 
+import Editor from "./Components/doc"; // text editing 
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // allows to go to different pages in the web-app
 import { app, database } from "./firebaseConfig";
 
 export default function App() {
   return (
     <div className="app">
       <h1>Welcome to Google Docs</h1>
-      <Routes>
-        // These are the different pages you can navigate to
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home database={database} />} />
-        <Route path="/editor/:id" element={<Editor database={database} />} />
+      <Routes> // These are the different pages you can navigate to
+        <Route path="/" element={<Login />} /> // Login page
+        <Route path="/home" element={<Home database={database} />} /> // home page
+        <Route path="/editor/:id" element={<Editor database={database} />} /> // editor page that opens up a 
+                                                                              // specific document w/ unique id
       </Routes>
     </div>
   );
